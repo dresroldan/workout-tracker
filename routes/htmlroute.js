2073d9843aab3da45f9ab58d02a1a6a1ad52f616
Andres Roldan <dresroldan@gmail.com>
2020-10-29 16:40:04 -0500

    added htmlroute-skeleton

---
+++ routes/htmlroute.js
@@ -0,0 +1,10 @@
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
+
+  
+
+
+
+
+module.exports = router;
+  
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
+var morgan = require('morgan')
 
 const PORT = process.env.PORT || 3090;
 
 const app = express();
+app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,6 +19,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 
 // routes
 app.use(require("./routes/api.js"));
+app.use(require("./routes/htmlroute.js"));
+
+
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
