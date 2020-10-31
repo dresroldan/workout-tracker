5a8f76feec4b26ea7ed29ad30b3248628e3648f1
Andres Roldan <dresroldan@gmail.com>
2020-10-30 22:45:46 -0500

    added workout model

--- models/workout.js
+++ models/workout.js
@@ -3,15 +3,37 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-   
+  day: {
+    type: Date,
+    default: Date.now,
+  },
 
+  exercises: [
+    {
+      type: {
+        type: "String",
+        trim: true,
+        required: "enter an exercise type",
+      },
 
-  });
-  
+      name: {
+        type: "String",
+        trim: true,
+        required: "enter an exercise name",
+      },
 
-  const Workout = mongoose.model("Workout", workoutSchema);
+      duration: {
+        type: Number,
+        required: "enter an exercise duration time",
+      },
+
+      weight: { type: Number },
+      reps: { type: Number },
+      sets: {type: Number },
+    },
+  ],
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
-
-
-
